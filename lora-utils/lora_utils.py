859075a975fa189a234fb959107eea086dc13c14
MikuAndRabbit <52323820+MikuAndRabbit@users.noreply.github.com>
2023-08-22 14:48:49 +0800

    Fix bug in lora_utils Modify the traversal method of model.named_modules()

--- lora-utils/lora_utils.py
+++ lora-utils/lora_utils.py
@@ -7,19 +7,15 @@ import copy
 
 def make_layer_lorable(layer: nn.Module, **lora_config):
     if isinstance(layer, nn.Linear):
-        lora_layer = lora.Linear(
-            layer.in_features, layer.out_features, **lora_config)
+        lora_layer = lora.Linear(layer.in_features, layer.out_features, **lora_config)
     elif isinstance(layer, nn.Conv2d):
         if len(layer.kernel_size) != 1:
             raise TypeError('The convolution kernel of Conv2d must be square.')
-        lora_layer = lora.Conv2d(
-            layer.in_channels, layer.out_channels, layer.kernel_size[0], **lora_config)
+        lora_layer = lora.Conv2d(layer.in_channels, layer.out_channels, layer.kernel_size[0], **lora_config)
     elif isinstance(layer, nn.Embedding):
-        lora_layer = lora.Embedding(
-            layer.num_embeddings, layer.embedding_dim, **lora_config)
+        lora_layer = lora.Embedding(layer.num_embeddings, layer.embedding_dim, **lora_config)
     else:
-        raise ValueError(
-            'LoRA only supports nn.Linear, nn.Conv2d, nn.Embedding.')
+        raise ValueError('LoRA only supports nn.Linear, nn.Conv2d, nn.Embedding.')
     return lora_layer
 
 
@@ -50,7 +46,7 @@ def make_lora_model_by_type(model: nn.Module, replace: List[str], lora_bias: str
     return model
 
 
-def make_lora_model_by_name(model: nn.Module, name_contains: List[str],  lora_bias: str, **lora_replace_config) -> nn.Module:
+def make_lora_model_by_name(model: nn.Module, name_contains: List[str], **lora_replace_config) -> nn.Module:
     model = copy.deepcopy(model)
 
     name2submodel = dict(model.named_modules())
