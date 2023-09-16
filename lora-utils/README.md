7975c29f52610c704cd439037c6f1221d0b3a864
MikuAndRabbit <52323820+MikuAndRabbit@users.noreply.github.com>
2023-09-16 12:22:40 +0800

    Update lora-utils

--- lora-utils/README.md
+++ lora-utils/README.md
@@ -1,6 +1,6 @@
 # 简介
 
-&emsp;&emsp;使用 `loralib` 库编写了用于替换模型中 `Linear`、`Conv2d` 和 `Embedding` 为对应的 LoRA 微调模块的相关工具方法。
+&emsp;&emsp;使用 `loralib` 库编写了用于替换模型中 `Linear`、`Conv` 和 `Embedding` 为对应的 LoRA 微调模块的相关工具方法。
 
 # 项目结构
 
--- lora-utils/lora_utils.py
+++ lora-utils/lora_utils.py
@@ -1,25 +1,57 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 import loralib as lora
 import torch.nn as nn
 import torch
 import copy
 
 
-def make_layer_lorable(layer: nn.Module, **lora_config):
-    if isinstance(layer, nn.Linear):
-        lora_layer = lora.Linear(layer.in_features, layer.out_features, **lora_config)
-    elif isinstance(layer, nn.Conv2d):
-        if len(layer.kernel_size) != 1:
-            raise TypeError('The convolution kernel of Conv2d must be square.')
-        lora_layer = lora.Conv2d(layer.in_channels, layer.out_channels, layer.kernel_size[0], **lora_config)
-    elif isinstance(layer, nn.Embedding):
-        lora_layer = lora.Embedding(layer.num_embeddings, layer.embedding_dim, **lora_config)
-    else:
-        raise ValueError('LoRA only supports nn.Linear, nn.Conv2d, nn.Embedding.')
-    return lora_layer
+class LoRALayerType(lora.LoRALayer, nn.Module):
+    pass
+
+
+def _get_other_conv_params(layer: nn.Module) -> Dict[str, Any]:
+    """Get the parameters of the convolutional layer except in_channels, out_channels, kernel_size.
+
+    Args:
+        layer (nn.Module): convolutional layer.
+
+    Returns:
+        Dict[str, Any]: other parameters of the convolutional layer.
+    """
+    assert isinstance(layer, (nn.Conv1d, nn.Conv2d, nn.Conv3d))
+    res = {
+        'stride': layer.stride,
+        'padding': layer.padding,
+        'dilation': layer.dilation,
+        'groups': layer.groups,
+        'bias': layer.bias is not None,
+        'padding_mode': layer.padding_mode
+    }
+    return res
+
+
+def _get_other_embedding_params(layer: nn.Embedding) -> Dict[str, Any]:
+    assert isinstance(layer, nn.Embedding)
+    res = {
+        'padding_idx': layer.padding_idx,
+        'max_norm': layer.max_norm,
+        'norm_type': layer.norm_type,
+        'scale_grad_by_freq': layer.scale_grad_by_freq,
+        'sparse': layer.sparse,
+        '_weight': layer.weight,
+        '_freeze': not layer.weight.requires_grad,
+    }
+    return res
 
 
 def _set_module(model: nn.Module, submodule_name: str, new_module: nn.Module):
+    """Replaces the sub-model with the specified name in the model.
+
+    Args:
+        model (nn.Module): model to be replaced.
+        submodule_name (str): submodel name.
+        new_module (nn.Module): new sub-model.
+    """    
     module_name_path = submodule_name.split('.')
     submodule_names = module_name_path[:-1]
     current_model = model
@@ -28,15 +60,58 @@ def _set_module(model: nn.Module, submodule_name: str, new_module: nn.Module):
     setattr(current_model, module_name_path[-1], new_module)
 
 
-def make_lora_model_by_type(model: nn.Module, replace: List[str], lora_bias: str, **lora_replace_config) -> nn.Module:
+def make_layer_lorable(layer: nn.Module, **lora_config) -> LoRALayerType:
+    """Returns the layer to which the LoRA is applied according to the type of layer passed in.
+
+    Args:
+        layer (nn.Module): layer that require LoRA to be applied.
+
+    Returns:
+        LoRALayer
+    """    
+    if isinstance(layer, nn.Linear):
+        lora_layer = lora.Linear(layer.in_features, layer.out_features, **lora_config)
+    elif isinstance(layer, (nn.Conv1d, nn.Conv2d, nn.Conv3d)):
+        if len(set(layer.kernel_size)) != 1:
+            raise TypeError('All dimensions of the convolution kernel of Conv must be the same.')
+        other_conv_params = _get_other_conv_params(layer)
+        if isinstance(layer, nn.Conv1d):            
+            lora_layer = lora.Conv1d(layer.in_channels, layer.out_channels, layer.kernel_size[0], **lora_config, **other_conv_params)
+        elif isinstance(layer, nn.Conv2d):
+            lora_layer = lora.Conv2d(layer.in_channels, layer.out_channels, layer.kernel_size[0], **lora_config, **other_conv_params)
+        elif isinstance(layer, nn.Conv3d):
+            lora_layer = lora.Conv3d(layer.in_channels, layer.out_channels, layer.kernel_size[0], **lora_config, **other_conv_params)
+    elif isinstance(layer, nn.Embedding):
+        other_embedding_params = _get_other_embedding_params(layer)
+        lora_layer = lora.Embedding(layer.num_embeddings, layer.embedding_dim, **lora_config, **other_embedding_params)
+    else:
+        raise ValueError('LoRA only supports nn.Linear, nn.Conv1d, nn.Conv2d, nn.Conv3d and nn.Embedding.')
+    return lora_layer
+
+
+def make_lora_model_by_type(model: nn.Module, types: List[str], **lora_config) -> nn.Module:
+    """Replaces the corresponding layer in the model with the corresponding layer in loralib based on the type passed in.
+
+    Args:
+        model (nn.Module): model to be replaced.
+        types (List[str]): types of layer to be replaced.
+        lora_bias (str): lora bias
+
+    Returns:
+        nn.Module: model after sub-model replacement.
+    """
     model = copy.deepcopy(model)
 
     replace_module_type = []
-    if 'linear' in replace:
+    if 'linear' in types:
         replace_module_type.append(nn.Linear)
-    if 'conv2d' in replace:
+    if 'conv1d' in types:
         replace_module_type.append(nn.Conv2d)
-    if 'embedding' in replace:
+    if 'conv2d' in types:
+        replace_module_type.append(nn.Conv2d)
+    if 'conv3d' in types:
+        replace_module_type.append(nn.Conv2d)
+    if 'embedding' in types:
         replace_module_type.append(nn.Embedding)
     if len(replace_module_type) == 0:
         return model
@@ -47,15 +122,22 @@ def make_lora_model_by_type(model: nn.Module, replace: List[str], lora_bias: str
     submodels_name = list(name2submodel.keys())
     for name in submodels_name:
         if isinstance(name2submodel[name], replace_module_type):
-            _set_module(model, name, make_layer_lorable(name2submodel[name], **lora_replace_config))
-
-    # only make lora layer trainable
-    lora.mark_only_lora_as_trainable(model, lora_bias)
+            _set_module(model, name, make_layer_lorable(name2submodel[name], **lora_config))
 
     return model
 
 
-def make_lora_model_by_name(model: nn.Module, name_contains: List[str], **lora_replace_config) -> nn.Module:
+def make_lora_model_by_name(model: nn.Module, name_contains: List[str], **lora_config) -> nn.Module:
+    """Replaces the corresponding layer in the model with the corresponding layer in loralib based on the type passed in.
+
+    Args:
+        model (nn.Module): model to be replaced.
+        name_contains (List[str]): names of layer to be replaced.
+        lora_bias (str): lora bias.
+
+    Returns:
+        nn.Module: model after sub-model replacement.
+    """
     model = copy.deepcopy(model)
 
     name2submodel = dict(model.named_modules())
@@ -63,7 +145,7 @@ def make_lora_model_by_name(model: nn.Module, name_contains: List[str], **lora_r
     for name in submodels_name:
         for need_replace_name in name_contains:
             if name.find(need_replace_name) != - 1:
-                _set_module(model, name, make_layer_lorable(name2submodel[name], **lora_replace_config))
+                _set_module(model, name, make_layer_lorable(name2submodel[name], **lora_config))
                 break
 
     return model
